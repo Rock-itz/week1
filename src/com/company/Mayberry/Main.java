a9ad482db77ca8c9dc86a58810c34aa94270ea01
12th CCHS Elijah J Mayberry <016994@SWCS.K12>
2020-01-14 14:46:28 -0500

    Initial commit

---
+++ src/com/company/Mayberry/Main.java
@@ -0,0 +1,34 @@
+package com.company.Mayberry;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    System.out.print("Hello World");
+
+
+    int age = 34;
+    double gpa = 3.2;
+    boolean isRaining = false;
+    String city = "Columbus";
+    System.out.print(city);
+    System.out.print(city.toUpperCase());
+
+
+    int dailyHighs[] ={50,60,70,80,90};
+
+
+/*    System.out.println(dailyHighs[0]);
+    System.out.println(dailyHighs[1]);
+    System.out.println(dailyHighs[2]);
+    System.out.println(dailyHighs[3]);
+    System.out.println(dailyHighs[4]);*/
+
+    for(int i = 0; i<=4; i++) {
+        System.out.println(twodA]);
+    }
+
+
+
+    }
+}
